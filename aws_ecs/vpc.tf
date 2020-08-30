739efa4dfd434102343f1159638ef721e57d9440
Sargis <sargis1177@gmail.com>
2020-08-30 18:00:47 +0400

    adding NAT and EI

--- aws_ecs/vpc.tf
+++ aws_ecs/vpc.tf
@@ -3,19 +3,10 @@ resource "aws_vpc" "ecs_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   tags = {
-    Name = "var.vpc"
+    Name = var.vpc
   }
 }
-# Define the public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id            = aws_vpc.ecs_vpc.id
-  cidr_block        = var.public_subnet_cidr
-  availability_zone = "us-east-2c"
 
-  tags = {
-    Name = var.public_subnet
-  }
-}
 
 # Define the private subnet
 resource "aws_subnet" "private_subnet" {
@@ -27,6 +18,23 @@ resource "aws_subnet" "private_subnet" {
     Name = var.private_subnet
   }
 }
+
+
+
+# Define the public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.ecs_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name = var.public_subnet
+  }
+}
+
+
+
+
 # Define the internet gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.ecs_vpc.id
@@ -36,101 +44,38 @@ resource "aws_internet_gateway" "gw" {
   }
 }
 
-# Define the route table
-resource "aws_route_table" "public_rt" {
+# Route the public subnet traffic through the IGW
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.ecs_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+
+# Create NAT Gateway with Elastic IP
+resource "aws_eip" "gw" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_nat_gateway" "gw" {
+  subnet_id     = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.gw.id
+}
+
+# Create a new route table for the private subnets, make it route non-local traffic through the NAT gateway to the internet
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.ecs_vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Public Subnet Route Table"
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.gw.id
   }
 }
 
-# Assign the route table to the public Subnet
-resource "aws_route_table_association" "public_rt" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
+# Assign the route table to the Private  Subnet
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
 }
 
-# Define the security group for public subnet
-resource "aws_security_group" "public_sg" {
-  name        = "public-subnet-sg"
-  description = "Allow incoming HTTP connections & SSH access"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = aws_vpc.ecs_vpc.id
-  tags = {
-    Name = "Public SG"
-  }
-
-}
-
-# Define the security group for private subnet
-resource "aws_security_group" "private_sg" {
-  name        = "private-subnet-sg"
-  description = "Allow traffic from public subnet"
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = [var.public_subnet_cidr]
-  }
-
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = [var.public_subnet_cidr]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.public_subnet_cidr]
-  }
-
-  vpc_id = aws_vpc.ecs_vpc.id
-
-  tags = {
-    Name = "Private SG"
-  }
-}
